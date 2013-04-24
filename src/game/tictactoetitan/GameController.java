0ad14f10c9bcdedc4a45ee37b36aeaa6ae6ad5e7
awesomegamer <avay_raul@hotmail.com>
2013-04-24 10:24:04 -0500

    deleted socket controller

--- src/game/tictactoetitan/GameController.java
+++ src/game/tictactoetitan/GameController.java
@@ -1,7 +1,5 @@
 package game.tictactoetitan;
 
-import java.net.MalformedURLException;
-
 import android.content.Intent;
 
 public class GameController {
@@ -15,11 +13,5 @@ public class GameController {
 		
 		Intent myIntent = new Intent(activity, GameActivity.class);
 		activity.startActivity(myIntent);
-		
-		try {
-			SocketController socket_controller = new SocketController();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
 	}
 }
